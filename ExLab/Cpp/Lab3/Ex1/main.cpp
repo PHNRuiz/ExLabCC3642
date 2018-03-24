13b2cbd7360e4f3b24cf23bd207c85e5423c7bba
Pedro Henrique Nardoni Ruiz <pedrohenriqueruiz@hotmail.com>
2018-03-23 21:40:06 -0300

    Add files via upload

---
+++ ExLab/Cpp/Lab3/Ex1/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+#include "Invoice.h"
+using namespace std;
+
+
+int main() {
+	Invoice i;//instanciando objeto
+	i.setId("123abc");//setando id do produto
+	i.setDesc("produto qualquer");//descriçao do                             //produto
+	i.setQuant(10);//há 10 produtos
+	i.setPreco(25);//custo de 25 por produto
+	i.getInvoiceAmount();//preço * quantidade
+	i.imprime();//impressao
+	
+}
